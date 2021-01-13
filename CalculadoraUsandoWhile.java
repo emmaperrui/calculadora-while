f3b35e9d950a7ad20509d72148d73a59a5a85631
Emma Perez <dinaisaur@gmail.com>
2021-01-13 09:57:47 +0100

    01 multiplos de 05

---
+++ CalculadoraUsandoWhile.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class CalculadoraUsandoWhile here.
+ * 
+ * @author (Emma) 
+ * @version (1)
+ */
+public class CalculadoraUsandoWhile
+{
+   /**
+    * Constructor for objects of class CalculadoraUsandoWhile
+    */
+   public void multiplosDeCinco(){
+       int index = 10;
+       while (index <= 95){
+           System.out.println(index);
+           index = index + 5;
+       }
+   }
+}
