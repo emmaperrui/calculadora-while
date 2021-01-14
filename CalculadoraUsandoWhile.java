cd2ed0a843e8800628311d8a3dffebbbed7385cc
Emma Perez <dinaisaur@gmail.com>
2021-01-14 08:42:28 +0100

    02 suma de valores

--- CalculadoraUsandoWhile.java
+++ CalculadoraUsandoWhile.java
@@ -7,14 +7,44 @@
  */
 public class CalculadoraUsandoWhile
 {
-   /**
-    * Constructor for objects of class CalculadoraUsandoWhile
-    */
-   public void multiplosDeCinco(){
-       int index = 15;
-       while (index <= 90){
-           System.out.println(index);
-           index = index + 5;
-       }
-   }
+    /**
+     * Constructor for objects of class CalculadoraUsandoWhile
+     */
+    public void multiplosDeCinco(){
+        int index = 15;
+        while (index <= 90){
+            System.out.println(index);
+            index = index + 5;
+        }
+    }
+
+    public void sumaDeValores (){
+        int contador = 1;
+        int suma = 0;
+        while (contador <= 10){
+            suma = suma + contador;
+            contador++;
+        }
+        System.out.println("El total de la suma es " + suma); 
+    }
+
+    public int sumaDeValoresEnIntervalo(int a, int b){
+        int resultado = 0;
+        if (a >= 0 && b >= 0){
+            if (b < a){
+                int c = a;
+                a = b;
+                b = c;
+            }
+            while (a <= b){
+                resultado = resultado + a;
+                a++;
+            }
+        }else {
+            System.out.println("Los números introducidos deben ser positivos");
+            resultado = -1;
+        }
+        return resultado;
+
+    }
 }
